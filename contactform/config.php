d468e192e0a5f1cfb7676ebdfecc1efe2e3ea730
Prafulbusa <busap@uw.edu>
2018-02-24 18:01:31 -0800

    Practice HTML, CSS, javaScript, & JQuery (Theme- Promoting hometown)

---
+++ contactform/config.php
@@ -0,0 +1,7 @@
+<?php
+    $empfaenger = "busap@uw.edu";  // Please enter your email address here. (between the inverted comma)
+    $ihrname = "Praful Busa"; // Please enter your name here. (between the inverted comma) It will appear as consigner in the thank mail.
+    
+    
+    $danke = "thankyou.php";  // URL to thank you page.
+?> 
---
+++ contactform/thankyou.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de-DE" lang="de-DE">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+<meta name="language" 			content="de"/>
+<meta name="description"      content="kontaktformular.com"/>
+<meta name="revisit"          content="After 7 days"/>
+<meta name="robots"           content="INDEX,FOLLOW"/>
+<meta http-equiv="Content-Style-Type" content="text/css" />   
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<title>kontaktformular.com</title>
+
+<style type="text/css">
+.kontaktformular  {
+	 width:  310px;
+     margin: 0px 0;
+     padding: 10px;
+     font-size: 11px;
+     font-family: Tahoma, Verdana, Arial;
+     border: 0px
+     background: #F5F5F5;
+     float: left;
+     clear: both;
+     }
+
+.kontaktformular fieldset {margin: 10px 0;}
+.kontaktformular a {color: #990000; text-decoration: none;}
+.kontaktformular a:hover {color: #483D8A;}
+  
+.kontaktformular legend {
+	 background: #006794;
+	 color: #fff;
+	 padding: 3px 5px;
+	 border: 1px solid #ddd;
+	 text-transform: uppercase;
+	 }
+
+.kontaktformular input:active, .kontaktformular input:focus, .kontaktformular input:hover { background: #ffff88;}
+.kontaktformular textarea:active, .kontaktformular textarea:focus, .kontaktformular textarea:hover { background: #ffff88;}
+
+.kontaktdaten table, .anfrage table, .captcha table { border-spacing:0px; width:290px; border:0px solid;}
+.label { width:100px; padding:0px;}
+.field { width:250px; padding:0px;}
+.error { width:200px; padding:0px 0px 0px 5px;}
+.captchareload { padding-left:10px;}
+input.errordesignfields, textarea.errordesignfields { background-color:#cc3333;}
+
+
+</style>
+</head>
+
+<body id="Kontaktformularseite">
+
+<div class="kontaktformular">
+
+ <fieldset class="kontaktdaten">
+    <legend>proof of delivery</legend>
+	<table>
+		<tr>
+			<td>Thank you very much. <br />Your message has been transfered succesfully.</td>		
+		</tr>
+	</table>
+  </fieldset><p>
+ <a href="http://www.kontaktformular.com/en" onclick="window.open(this.href); return false;" title="www.kontaktformular.com"><!-- It is not allowed to remove this copyright notice. -->&copy; by Pierce Search and Rescue Council - All rights reserved.</a></p>
+
+</div>
+
+</body>
+</html>
